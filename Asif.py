feb87f07fa2f11ce79d42f22351f4d1eb02284ce
😂TERA ABU JAN🥵 <walilila40@gmail.com>
2026-09-23 18:41:53 +0300

    Asif.py

--- Asif.py
+++ Asif.py
@@ -1,3 +1,14 @@
-import BNBB
+import sys
 
-BNBB.main()
+if sys.version_info[:2] == (3, 11):
+    import BNBM
+    BNBM.main()
+
+elif sys.version_info[:2] == (3, 13):
+    import BNBB
+    BNBB.main()
+
+else:
+    raise RuntimeError(
+        f"Unsupported Python version: {sys.version_info.major}.{sys.version_info.minor}"
+    )
